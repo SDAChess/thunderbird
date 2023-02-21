033e6880a710d9b1701c75b38b921bbbb220c2a6
Simon Scatton <simon.scatton@epita.fr>
2023-02-21 22:40:42 +0100

    init: fixed thunderbird version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
   inputs = {
     nixpkgs = {
-      url = "github:nixos/nixpkgs?rev=3cdefadeaf6bfbb6b52ee7798152b492c263d311";
+      url = "github:nixos/nixpkgs?rev=318fae87c0b61eb2eabe6d96b5824cc6e282c513";
       flake = false;
     };
     flake-utils = { url = "github:numtide/flake-utils"; };
